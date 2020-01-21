dfeaa4d11c31f7478c928990d3337675334ea85f
giogio <francesco.gionghi@gmail.com>
2020-01-21 15:01:10 +0100

    adding java project for example

---
+++ k8s-gitlab/kubeadm script/up2.sh
@@ -0,0 +1,45 @@
+yum install -y yum-utils device-mapper-persistent-data lvm2
+
+yum-config-manager \
+  --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+
+yum update && yum install -y docker-ce-18.06.2.ce
+
+cat > /etc/docker/daemon.json <<EOF
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+mkdir -p /etc/systemd/system/docker.service.d
+
+
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+
+yum install -y net-tools kubelet kubeadm --nogpgcheck --disableexcludes=kubernetes
+
+systemctl enable kubelet
+systemctl enable docker && systemctl start docker
+
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=enforcing/g' /etc/selinux/config
+swapoff -a
+sed -i '/swap/s/^/#/g' /etc/fstab
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+modprobe br_netfilter
+

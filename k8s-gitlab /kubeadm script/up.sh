69ceacb9d8b98c51d542f2877cde72a3c681e2fe
Francesco Gionghi <francesco.gionghi@gmail.com>
2020-01-22 10:04:30 +0100

    Deleting doubled folder

--- k8s-gitlab /kubeadm script/up.sh
+++
@@ -1,7 +0,0 @@
-ON MASTER:
-kubeadm init --apiserver-advertise-address=192.168.60.101 --pod-network-cidr=10.244.0.0/16
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134e0ed5af15c65e414cf26827915/Documentation/kube-flannel.yml
-JOIN THE WORKER
-ON ALL NODES:
-Add ~--node-ip~ flag in ~/var/lib/kubelet/kubeadm-flags.env~ gives no error
-
